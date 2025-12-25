7021b82e35e2d43d68bae8ee2b50f19a6911bb24
Sayed Nafisur Rahman Alif <143423207+SayedAliff@users.noreply.github.com>
2025-12-26 00:45:30 +0600

    fixxxx

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -1,64 +1,69 @@
 "use client"
 
 import * as React from "react"
+
+
 import { GripVertical } from "lucide-react"
-import {
-  PanelGroup,
-  Panel,
-  PanelResizeHandle,
-  type ImperativePanelHandle,
-} from "react-resizable-panels"
 
 import { cn } from "./utils"
 
-interface ResizablePanelGroupProps
-  extends React.ComponentProps<typeof PanelGroup> {}
+interface ResizablePanelGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+  direction?: "horizontal" | "vertical"
+}
 
-const ResizablePanelGroup = React.forwardRef<
-  ImperativePanelHandle,
-  ResizablePanelGroupProps
->(({ className, ... props }, ref) => (
-  <PanelGroup
-    className={cn(
-      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-      className
-    )}
-    {... props}
-  />
-))
-ResizablePanelGroup. displayName = "ResizablePanelGroup"
+const ResizablePanelGroup = React.forwardRef<HTMLDivElement, ResizablePanelGroupProps>(
+  ({ className, direction = "horizontal", ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "flex h-full w-full",
+        direction === "vertical" && "flex-col",
+        className
+      )}
+      {...props}
+    />
+  )
+)
+ResizablePanelGroup.displayName = "ResizablePanelGroup"
 
-interface ResizablePanelProps extends React.ComponentProps<typeof Panel> {}
+interface ResizablePanelProps extends React.HTMLAttributes<HTMLDivElement> {
+  defaultSize?: number
+  minSize?:  number
+  maxSize?: number
+}
 
-const ResizablePanel = React.forwardRef<
-  ImperativePanelHandle,
-  ResizablePanelProps
->((props, ref) => <Panel ref={ref} {...props} />)
+const ResizablePanel = React.forwardRef<HTMLDivElement, ResizablePanelProps>(
+  ({ className, ... props }, ref) => (
+    <div
+      ref={ref}
+      className={cn("flex-1", className)}
+      {...props}
+    />
+  )
+)
 ResizablePanel.displayName = "ResizablePanel"
 
-interface ResizableHandleProps
-  extends React.ComponentProps<typeof PanelResizeHandle> {
+interface ResizableHandleProps extends React.HTMLAttributes<HTMLDivElement> {
   withHandle?: boolean
 }
 
-const ResizableHandle = ({
-  withHandle,
-  className,
-  ...props
-}: ResizableHandleProps) => (
-  <PanelResizeHandle
-    className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after: w-1 after:-translate-x-1/2 focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after: h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-      className
-    )}
-    {...props}
-  >
-    {withHandle && (
-      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
-        <GripVertical className="h-2. 5 w-2.5" />
-      </div>
-    )}
-  </PanelResizeHandle>
+const ResizableHandle = React.forwardRef<HTMLDivElement, ResizableHandleProps>(
+  ({ withHandle, className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1",
+        className
+      )}
+      {...props}
+    >
+      {withHandle && (
+        <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
+          <GripVertical className="h-2.5 w-2.5" />
+        </div>
+      )}
+    </div>
+  )
 )
 ResizableHandle.displayName = "ResizableHandle"
 
